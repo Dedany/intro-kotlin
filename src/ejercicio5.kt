71bccb256b72436eea0e79babc2427e1a19967f0
Danielperez <danylerez@gmail.com>
2024-12-02 11:54:12 +0100

    estoy teniendo problemas con este ejercicio

---
+++ src/ejercicio5.kt
@@ -0,0 +1,12 @@
+//Crea una función que calcule el porcentaje de victorias de un equipo de futbol
+// según los partidos que ha jugado y los que ha ganado
+
+class ejercicio5 {
+
+        val a= 5f
+        val b= 3f
+
+        val division = if (b != 0f) a / b else null
+
+        println(division?: "error: no se puede dividir por cero")
+}
