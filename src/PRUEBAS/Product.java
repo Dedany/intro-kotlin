cdce63be58a8dd87cc2b7c9c2dd6d194fac74873
Danielperez <danylerez@gmail.com>
2024-12-05 12:05:51 +0100

    Update add class Product

---
+++ src/PRUEBAS/Product.java
@@ -0,0 +1,14 @@
+package PRUEBAS;
+
+class Product(
+        /**Attributes **/
+
+        var id: Int,
+        var name: String,
+        var quantity: Float,
+        var reference: String ,
+        var image: String,
+        var description: String,
+        )
+
+
