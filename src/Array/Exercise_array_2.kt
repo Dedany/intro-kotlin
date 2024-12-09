dbe271d67667162e0de8cadf4d2e47f76c184da8
Danielperez <danylerez@gmail.com>
2024-12-09 10:27:29 +0100

    Update Exercise_2

---
+++ src/Array/Exercise_array_2.kt
@@ -0,0 +1,34 @@
+package Array
+
+//Crea una función que dado un conjunto de enteros devuelva
+// si existe un número dentro de él
+fun InserNum2(array: List<Int>) {
+
+
+    println("inserta numero para ver si está en el array ")
+
+    val input= readLine()?.toIntOrNull()
+
+    if (input==null){
+        println("introduce un numero válido")
+        return
+    }
+    if (input in array) {
+        println("el numero está dentro")
+    }
+   else{
+       println("no está dentro")
+    }
+
+
+
+
+
+
+}
+
+fun main(){
+ val array = listOf(1,2,3,4)
+
+    InserNum2(array)
+}
