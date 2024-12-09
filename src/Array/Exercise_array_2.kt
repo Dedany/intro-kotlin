f47539969765452356f399328a3ae50cdcf34fcc
Danielperez <danylerez@gmail.com>
2024-12-09 10:29:16 +0100

    update

--- src/Array/Exercise_array_2.kt
+++ src/Array/Exercise_array_2.kt
@@ -7,28 +7,23 @@ fun InserNum2(array: List<Int>) {
 
     println("inserta numero para ver si está en el array ")
 
-    val input= readLine()?.toIntOrNull()
+    val input = readLine()?.toIntOrNull()
 
-    if (input==null){
+    if (input == null) {
         println("introduce un numero válido")
         return
     }
     if (input in array) {
         println("el numero está dentro")
+    } else {
+        println("no está dentro")
     }
-   else{
-       println("no está dentro")
-    }
 
-
-
-
-
-
+    
 }
 
-fun main(){
- val array = listOf(1,2,3,4)
+fun main() {
+    val array = listOf(1, 2, 3, 4)
 
     InserNum2(array)
 }
